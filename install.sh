bef90631c800e5a31b86f9b168a9fadb26b31358
James Wang <jameswang9909@hotmail.com>
2015-10-10 19:02:09 -0400

    Do not add alias if it already exists

--- install.sh
+++ install.sh
@@ -30,13 +30,15 @@ run_with_status sudo cp settings.conf ~/.homeworkserver/settings.conf
 cd
 
 if [ -e ".bash_aliases" ]; then
-  run_with_status sudo cp .bash_aliases alias_data
-  echo "alias homeworkserver='~/.homeworkserver/homeworkserver'" | sudo tee -a alias_data
-  run_with_status sudo mv alias_data .bash_aliases
+    if [[ $(grep "homeworkserver" .bash_aliases) == "" ]]; then
+        run_with_status sudo cp .bash_aliases alias_data
+        echo "alias homeworkserver='~/.homeworkserver/homeworkserver'" | sudo tee -a alias_data
+        run_with_status sudo mv alias_data .bash_aliases
+    fi
 else
-  run_with_status touch alias_data
-  echo "alias homeworkserver='~/.homeworkserver/homeworkserver'" | sudo tee -a alias_data
-  run_with_status sudo mv alias_data .bash_aliases
+    run_with_status touch alias_data
+    echo "alias homeworkserver='~/.homeworkserver/homeworkserver'" | sudo tee -a alias_data
+    run_with_status sudo mv alias_data .bash_aliases
 fi
 source .bashrc
 echo Successfully installed HomeworkServer
